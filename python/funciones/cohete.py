37dbd1d4a68b8bfe223a634a7802d9e574fb6fab
b4stiann <bastian.rubilar.soto@alumnos.sip.cl>
2025-04-16 10:41:54 -0400

    changes1

---
+++ python/funciones/cohete.py
@@ -0,0 +1,8 @@
+#Bastian Rubilar
+#16-04-2025
+
+def distancia_a_millas(distancia):
+    millas = distancia / 1.609
+    print(f"La distancia en millas es: {millas:.2f} millas")
+
+distancia_a_millas(10000)
